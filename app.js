43c46f6f7096998359b78b1c1623896bb24634bd
farritpc <farritzt@hotmail.com>
2020-04-13 23:31:28 +0700

    update datajson

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express();
+const fs = require('fs')
 app.set('view engine','ejs');
 app.set('views','view');
 app.get('/',function(req,res){
-    res.render('index',{name:'farrit'});
+    fs.readFile('./data.json',(err,data)=>{
+        const listobj = JSON.parse(data);
+        if(err){
+            res.status(400).send('error list not found');
+        }else{
+            res.render('index',{lists:listobj})
+        }
+    })
 });
 app.listen(3000,function(){
-    console.log('Listening on port 300');
+    console.log('Listening on port 3000');
 });
