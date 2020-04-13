db924f229037244cd1ea978a9990149bd04dcf53
farritpc <farritzt@hotmail.com>
2020-04-13 23:10:33 +0700

    ejs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# express
- hello express
+# Nodejs-express-exemple
+
+ use express for web server
+ use EJS for template Engine
--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const expressfunction = require('express');
-const expressApp = expressfunction();
-
-expressApp.get('/',funtion(req,res){
-	res.send('Hello worlld');
-})
+const express = require('express')
+const app = express();
+app.set('view engine','ejs');
+app.set('views','view');
+app.get('/',function(req,res){
+    res.render('index',{name:'farrit'});
+});
+app.listen(3000,function(){
+    console.log('Listening on port 300');
+});
